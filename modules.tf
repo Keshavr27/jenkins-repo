13f76ad23a5c352025b3fe459c2ec0649ab31a33
Keshavr27 <51294368+Keshavr27@users.noreply.github.com>
2025-12-08 23:06:25 +0530

    Add Azure resource VM to modules.tf

--- modules.tf
+++ modules.tf
@@ -2,3 +2,8 @@ resource "azurerm_resource_group" "example" {
   name     = "example"
   location = "West Europe"
 }
+resource "azurerm_resource_vm" "example" {
+  name     = "example-vm"
+  location = "West Europe"
+}
+
